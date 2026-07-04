2dea78040fecd165b0ff038b9add60e08755986d
kumarhemant96979-arch <kumarhemant96979@gmail.com>
2026-07-04 22:49:19 +0530

    Added backend logic for simple authentic system (security=3/10)

--- authsys/views.py
+++ authsys/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from authsys.models import *
 from .decorator import *
 # Create your views here.
+
+def signin(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        email=request.POST.get('emial')
+
+        User.objects.create(username=username,password=password,email=email)
+        return redirect('login')
+    
+    return render(request,'signup_page.html')
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        email=request.POST.get('emial')
+
+        user=User.objects.filter(username=username,password=password,email=email).first()
+
+        if user:
+            request.session['user_id']=user.id
+        return redirect('home')
+    
+    return render(request,'login_page.html')
+
+def logout(request):
+    request.session.flush()
+    return redirect('login')
+
+def delete_account(request):
+    user_id=request.session.get('user_id')
+    user=User.session.get(id=user_id)
+    request.session.flush()
+    user.delete()
+    return redirect('signin')
+
+
+
 
